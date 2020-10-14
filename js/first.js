4d03c46aedf6fa021d9d8871b3fd7927d96a071d
laith <67703195+laith206@users.noreply.github.com>
2020-10-14 20:19:11 +0300

    Update first.js

--- js/first.js
+++ js/first.js
@@ -17,24 +17,30 @@ var productlist;
 
 function objectss() {
     var aaa = "";
+
     if (productNameIput.value !== "" & producturl.value !== "") {
         document.getElementById('alertttt').innerHTML = "";
 
         var product = {
             name: productNameIput.value,
             url: producturl.value,
+
         };
+
         productlist.push(product)
         console.log(productlist)
 
         localStorage.setItem('ourbroduct', JSON.stringify(productlist))
 
         dispalyproductlist()
+        productNameIput.value = ""
+        producturl.value = ""
     } else {
         var aaa =
             `
          <div id="alertttt" class="alert alert-danger ">
-             input name and url
+            <h3 class=" text text-danger">warning</h3><h4 class="text text-dark" >input name and url</h4>
+
           </div>
 
         `
@@ -50,10 +56,12 @@ function dispalyproductlist() {
             `
         <tr id="${i}">
         <td>${i}</<td>
-        <td>${productlist[i].name}</<td>
+        <td id="pro${i}">${productlist[i].name}</<td>
         <td>${productlist[i].url}</<td>
         <td> <a class='btn btn-warning' href='${productlist[i].url}'>viste</a></<td> 
         <td><button  class='btn btn-danger'onclick = 'deleterow(${i})'>Delete</button></<td>
+        <td><button  class='btn btn-danger'onclick = 'ciiiiih("${productlist[i].name}")'>update</button></<td>
+
         </tr>
 
         `
@@ -63,12 +71,41 @@ function dispalyproductlist() {
 }
 
 
+function ciiiiih(name) {
+    //var tabl = name
+    //fore(var i = 0; i < tabl.length; i++) {
+    //document.getElementById("sch").outerHTML
+    //}
+    //document.getElementById('productNameInpyt').value = name;
+    //alert(name & name2)
+    //alert(name2)
+
+    productlist = JSON.parse(localStorage.getItem('ourbroduct'));
+    productlist.forEach(tabl => {
+
+        if (tabl.name == name | tabl.name === name) {
+
+            tabl.name = document.getElementById('productNameInpyt').value;
+            // 
+            console.log(tabl)
+        }
+
+    });
+    localStorage.setItem('ourbroduct', JSON.stringify(productlist)); //put the object back
+    dispalyproductlist()
+
+}
+
 function deleterow(row) {
     productlist = JSON.parse(localStorage.getItem('ourbroduct'));
     productlist.pop(row)
     localStorage.setItem('ourbroduct', JSON.stringify(productlist)); //put the object back
     document.getElementById(row).innerHTML = "";
 }
+
+
+
+
 
 
 
